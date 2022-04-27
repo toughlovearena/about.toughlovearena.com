56337529914997241e0aad6645039c665f10d0ac
mpaulweeks <mpaulweeks@gmail.com>
2022-04-27 13:23:43 -0400

    add logo and redesign header

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,6 +1,7 @@
 import React, { ReactNode } from 'react'
 import Link from 'next/link'
 import Head from 'next/head'
+import styles from './Layout.module.css';
 
 type Props = {
   children?: ReactNode
@@ -17,19 +18,18 @@ const Layout = ({ children, title = 'This is the default title' }: Props) => (
       <link rel="stylesheet" href="/styles.css" />
     </Head>
     <header>
-      <nav>
+      <nav className={styles.nav}>
         <Link href="/">
           <a>Home</a>
-        </Link>{' '}
-        |{' '}
+        </Link>
         <Link href="/press">
           <a>Press</a>
-        </Link>{' '}
-        |{' '}
+        </Link>
+        <img src="/logo_vert.png" />
         <Link href="/users">
           <a>Users List</a>
-        </Link>{' '}
-        | <a href="/api/users">Users API</a>
+        </Link>
+        <a href="/api/users">Users API</a>
       </nav>
     </header>
     {children}
