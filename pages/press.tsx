fc83f963716f714b92604bc5f075b87d939e92d7
mpaulweeks <mpaulweeks@gmail.com>
2022-04-27 18:25:56 -0400

    add sorting utils

--- pages/press.tsx
+++ pages/press.tsx
@@ -2,32 +2,36 @@ import Link from 'next/link'
 import { Column } from '../components/Column';
 import Layout from '../components/Layout'
 import { Articles } from '../data/links';
+import { sortArrayOfObjects } from '../utils/list';
 
-const PressPage = () => (
-  <Layout title="Press">
-    <Column>
-      <h1>Press</h1>
-      <div>
-        <a href="/presskit-toughlovearena-2022-04-26.zip">
-          Download Presskit
-        </a>
-      </div>
-      <h2>
-        Praise for Tough Love Arena
-      </h2>
-      {Articles.map(ext => (
-        <p>
-          {ext.author}
-          <br />
-          <a href={ext.url} target="_blank">
-            {ext.label}
+const PressPage = () => {
+  const descending = sortArrayOfObjects(Articles, art => art.date).reverse();
+  return (
+    <Layout title="Press">
+      <Column>
+        <h1>Press</h1>
+        <div>
+          <a href="/presskit-toughlovearena-2022-04-26.zip">
+            Download Presskit
           </a>
-          <br />
-          {ext.date}
-        </p>
-      ))}
-    </Column>
-  </Layout>
-);
+        </div>
+        <h2>
+          Praise for Tough Love Arena
+        </h2>
+        {descending.map(ext => (
+          <p>
+            {ext.author}
+            <br />
+            <a href={ext.url} target="_blank">
+              {ext.label}
+            </a>
+            <br />
+            {ext.date}
+          </p>
+        ))}
+      </Column>
+    </Layout>
+  );
+};
 
 export default PressPage;
