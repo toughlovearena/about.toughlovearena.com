0525b21ae9d3343b733ad3cb967c5c6b6ada8316
mpaulweeks <mpaulweeks@gmail.com>
2022-04-27 19:17:36 -0400

    add youtube embed logic

--- pages/press.tsx
+++ pages/press.tsx
@@ -11,6 +11,20 @@ const ExternalLink = (props: { children: ReactNode; }) => (
   </div>
 )
 
+const YouTubeEmbed = (props: { url: string; }) => {
+  return (
+    <iframe
+      width="560"
+      height="315"
+      src={props.url}
+      title="YouTube video player"
+      frameBorder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+      allowFullScreen>
+    </iframe>
+  )
+};
+
 const PressPage = () => {
   const articles = sortArrayOfObjects(Articles, art => art.date).reverse();
   const videos = sortArrayOfObjects(YouTubeVideos, art => art.date).reverse();
