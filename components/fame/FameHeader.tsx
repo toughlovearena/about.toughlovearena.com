9699896e317ab6e64e1641c3813ffcd14e94202e
mpaulweeks <mpaulweeks@gmail.com>
2022-05-05 16:03:24 -0400

    convert FameHeader styles

--- components/fame/FameHeader.tsx
+++ components/fame/FameHeader.tsx
@@ -1,52 +1,29 @@
-import styled from 'styled-components';
 import { CONSTANTS } from '../../data/constants';
 import { HallOfFameData } from '../../interfaces';
 import { sortArray } from '../../utils/list';
-
-const HeaderDiv = styled.div`
-  padding: 1em;
-  text-align: center;
-  font-size: 1.5em;
-
-  a {
-    color: var(--heart);
-  }
-`;
-
-const StatsTitle = styled.div`
-  font-size: 2em;
-`;
-const SubTitle = styled.div`
-  font-size: 1em;
-  margin: 1em 0;
-`;
-const Updated = styled.div`
-  width: 100%;
-  text-align: right;
-  font-style: italic;
-  font-size: 0.8em;
-`;
-const SocialInfo = styled.div`
-  font-size: 0.8em;
-`;
+import styles from './FameHeader.module.css'
 
 export const FameHeader = (props: { fame: HallOfFameData, }) => {
   const updated = sortArray(props.fame.events.map((evt) => evt.date)).reverse()[0];
   return (
-    <HeaderDiv>
-      <StatsTitle>The Hall of Fame</StatsTitle>
-      <Updated>last updated {updated}</Updated>
-      <SubTitle>
+    <div className={styles.header}>
+      <div className={styles.title}>
+        The Hall of Fame
+      </div>
+      <div className={styles.updated}>
+        last updated {updated}
+      </div>
+      <div className={styles.subtitle}>
         Here we celebrate and immortalize all of the{" "}
         <a href={CONSTANTS.URLs.Game}>Tough Love Arena</a> champions
-      </SubTitle>
-      <SocialInfo>
+      </div>
+      <div className={styles.social}>
         Want to submit your event to the Hall of Fame?
         <br />
         Send us an <a href={CONSTANTS.URLs.Email}>email</a>, message us on{" "}
         <a href={CONSTANTS.URLs.Twitter}>Twitter</a>, or fill out our{" "}
         <a href={CONSTANTS.URLs.Form}>Google Form</a>
-      </SocialInfo>
-    </HeaderDiv>
+      </div>
+    </div>
   );
 }
