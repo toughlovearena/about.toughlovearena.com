482f19dd898e26ca791940fc06b21ab29863a795
mpaulweeks <mpaulweeks@gmail.com>
2022-05-05 23:22:32 -0400

    simplify hall of fame header

--- components/fame/FameHeader.tsx
+++ components/fame/FameHeader.tsx
@@ -6,15 +6,11 @@ export const FameHeader = (props: { fame: HallOfFameData, }) => {
   return (
     <div className={styles.header}>
       <div className={styles.subtitle}>
-        Here we celebrate and immortalize all of the{" "}
-        <a href={CONSTANTS.URLs.Game}>Tough Love Arena</a> champions
+        Here we celebrate and immortalize all of the Tough Love Arena champions
       </div>
       <div className={styles.social}>
         Want to submit your event to the Hall of Fame?
-        <br />
-        Send us an <a href={CONSTANTS.URLs.Email}>email</a>, message us on{" "}
-        <a href={CONSTANTS.URLs.Twitter}>Twitter</a>, or fill out our{" "}
-        <a href={CONSTANTS.URLs.Form}>Google Form</a>
+        Fill out this <a href={CONSTANTS.URLs.Form}>form</a>.
       </div>
     </div>
   );
