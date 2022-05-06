36459d3336e74059db32c42b54caa680eaca84ce
mpaulweeks <mpaulweeks@gmail.com>
2022-05-05 23:22:21 -0400

    fix styling on index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,8 @@
 import Hero from '../components/Hero'
 import Layout from '../components/Layout'
-import { InternalPage } from '../data/nav'
 
 const IndexPage = () => (
-  <Layout page={InternalPage.About}>
+  <Layout>
     <Hero />
   </Layout>
 )
