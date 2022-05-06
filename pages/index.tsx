9d26ad8f62f724a7ec84a8b66df13adec5e62d94
mpaulweeks <mpaulweeks@gmail.com>
2022-05-05 22:32:15 -0400

    delete remaining starter code

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,3 @@
-import Link from 'next/link'
-import { Column } from '../components/Column'
 import Hero from '../components/Hero'
 import Layout from '../components/Layout'
 import { InternalPage } from '../data/nav'
@@ -7,19 +5,6 @@ import { InternalPage } from '../data/nav'
 const IndexPage = () => (
   <Layout page={InternalPage.About}>
     <Hero />
-    <Column>
-      <h3>Testing, please ignore</h3>
-      <div>
-        <Link href="/users">
-          <a>Users List</a>
-        </Link>
-      </div>
-      <div>
-        <a href="/api/users">
-          Users API
-        </a>
-      </div>
-    </Column>
   </Layout>
 )
 
