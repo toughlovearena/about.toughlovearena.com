2af3459df5bc01322ff09f909e2d7363d5dbe3df
mpaulweeks <mpaulweeks@gmail.com>
2022-05-16 22:11:30 -0400

    add pull quotes

--- data/links.ts
+++ data/links.ts
@@ -6,30 +6,35 @@ export const Articles: ArticleLink[] = [{
   author: 'Kenneth Shepard',
   title: 'Six of the Coolest Games We Played at PAX East 2022',
   url: 'https://www.fanbyte.com/features/six-of-the-coolest-games-we-played-at-pax-east-2022/',
+  quote: `It’s been awhile since I’ve seen a game so wonderfully break down fighting game mechanics to their rawest form in a way that wasn’t gimmicky.`,
 }, {
   date: '2022-04-23',
   site: 'The Mary Sue',
   author: 'Ana Valens',
   title: `I Wish Tough Love Arena Was the First Fighting Game I Ever Played`,
   url: 'https://www.themarysue.com/i-wish-tough-love-arena-was-the-first-fighting-game-i-ever-played/',
+  quote: `Tough Love Arena has accomplished something rare in the fighting game community: It’s truly easy to learn, yet difficult to master, with plenty of potential for both casual and competitive multiplayer play.`,
 }, {
   date: '2021-02-21',
   site: 'frostilyte',
   author: 'MRMKL',
   title: 'Fighting Games and Approachable Design',
   url: 'https://frostilyte.ca/2021/02/18/fighting-games-and-approachable-design-spaces/',
+  quote: `Limiting the volume of knowledge required, while still allowing for unique playstyles, is a worthwhile approachability goal and is well served here.`,
 }, {
   date: '2021-02-08',
   site: 'Vice',
   author: 'Eric Van Allen',
   title: `Tough Love Arena Feels Like a Throwback to Computer Lab Emulator Bouts`,
   url: 'https://www.vice.com/en/article/epd8vk/tough-love-arena-browser-fighting-game-rollback-netcode',
+  quote: `In a post-Flash world, it's nice to be able to send a friend a link and say, "Let's play a round." So here's the link, if you want to meet up at the great computer lab in the web sometime.`,
 }, {
   date: '2021-01-06',
   site: 'The Gamer',
   author: 'Sam Butler',
   title: 'Tough Love Arena Is A Free To Play Fighting Game With Rollback Netcode',
   url: 'https://www.thegamer.com/tough-love-arena-free-to-play-fighting-game-rollback-netcode/',
+  quote: `It is a simple game to get into, with the depth to explore expected from a fighting game. Its unique visuals and style should set it apart from the other fighting games, and having rollback netcode will help with smooth online matches.`,
 }];
 
 export const YouTubeVideos: YouTubeLink[] = [{
