fa52214d90f3b707c3ab4846ed5dd3e290c5393d
mpaulweeks <mpaulweeks@gmail.com>
2022-05-17 00:34:06 -0400

    change height of hero text, add background box to quote

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,15 +1,15 @@
-import { CSSProperties, useCallback, useEffect, useState } from 'react';
+import { CSSProperties, useCallback, useEffect, useRef, useState } from 'react';
 import { Articles } from '../data/links';
 import { sleep } from '../utils/async';
 import { getNextInArray, shuffleArray } from '../utils/list';
 import styles from './Hero.module.css';
 
 export default () => {
-  const shuffled = shuffleArray(Articles);
+  const shuffled = useRef(shuffleArray(Articles));
   const [article, setArticle] = useState(Articles[0]);
   const [visible, setVisible] = useState(true);
 
-  const transitionMs = 500;
+  const transitionMs = 300;
   const activeMs = 10 * 1000;
 
   const cycle = useCallback(() => {
@@ -32,16 +32,16 @@ export default () => {
         // wait for transition
         await sleep(2 * transitionMs);
         // change article
-        setArticle(current => getNextInArray(current, shuffled));
+        setArticle(current => getNextInArray(current, shuffled.current));
       }
     })();
-  }, [visible]);
+  }, [shuffled, visible]);
 
   const quoteStyle: CSSProperties = {
     transitionDuration: transitionMs + 'ms',
     ...(visible ? {} : {
       opacity: 0,
-      transform: 'translate(0%, 1em)',
+      transform: 'translate(0%, 0.5em)',
     })
   };
 
@@ -56,7 +56,7 @@ export default () => {
             {article.quote}
           </div>
           <div className={styles.quoteAuthor}>
-            - {article.author} [<a href={article.url}>{article.site}</a>]
+            {article.author} [<a href={article.url}>{article.site}</a>]
           </div>
         </div>
       </div>
