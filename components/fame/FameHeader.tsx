5e62b7b95e0d294ff5d0e956f7d3e99aafdad391
mpaulweeks <mpaulweeks@gmail.com>
2022-05-21 19:03:40 -0400

    style fame header

--- components/fame/FameHeader.tsx
+++ components/fame/FameHeader.tsx
@@ -4,10 +4,8 @@ import styles from './FameHeader.module.css'
 export const FameHeader = () => {
   return (
     <div className={styles.header}>
-      <div className={styles.social}>
-        Want to submit your event to the Hall of Fame?
-        Fill out this <a href={CONSTANTS.URLs.Form}>form</a>.
-      </div>
+      Want to submit your event to the Hall of Fame?
+      Fill out this <a href={CONSTANTS.URLs.Form}>form</a>.
     </div>
   );
 }
