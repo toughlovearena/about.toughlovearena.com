3eef79cf1006d0b83eafee389cff7b31884ad2b0
mpaulweeks <mpaulweeks@gmail.com>
2022-05-25 15:16:28 -0400

    fix navmobile appearing on desktop, fix hero on mobile

--- components/layout/NavMobile.tsx
+++ components/layout/NavMobile.tsx
@@ -4,24 +4,26 @@ import { NavProps } from '../../interfaces';
 import styles from './NavMobile.module.css';
 
 export default ({ currentHref, links, }: NavProps) => (
-  <nav className={styles.nav}>
-    <div className={styles.navLogo}>
-      <Link href={InternalPage.Home.href}>
-        <img src="/logo_v01.png" />
-      </Link>
-    </div>
-    <div className={styles.navLinks}>
-      {links.map((li, i) => (
-        <Link key={i} href={li.href}>
-          <div className={styles.navLink}>
-            {currentHref === li.href ? (
-              <b>{li.label}</b>
-            ) : (
-              <span>{li.label}</span>
-            )}
-          </div>
+  <div className={styles.container}>
+    <nav className={styles.nav}>
+      <div className={styles.navLogo}>
+        <Link href={InternalPage.Home.href}>
+          <img src="/logo_v01.png" />
         </Link>
-      ))}
-    </div>
-  </nav>
+      </div>
+      <div className={styles.navLinks}>
+        {links.map((li, i) => (
+          <Link key={i} href={li.href}>
+            <div className={styles.navLink}>
+              {currentHref === li.href ? (
+                <b>{li.label}</b>
+              ) : (
+                <span>{li.label}</span>
+              )}
+            </div>
+          </Link>
+        ))}
+      </div>
+    </nav>
+  </div>
 )
