03a1fbfb39271a180cdd33f9498ae3d66d77e918
mpaulweeks <mpaulweeks@gmail.com>
2022-05-26 10:47:21 -0500

    fix ssr bug

--- utils/useDocumentSize.ts
+++ utils/useDocumentSize.ts
@@ -1,7 +1,13 @@
 import { useCallback, useEffect, useState } from "react";
 
-export function useDocumentSize() {
-  const [size, setSize] = useState(document.body.getBoundingClientRect());
+interface Size {
+  height: number;
+  width: number;
+}
+
+export function useDocumentSize(): Size {
+  // cannot reference document.body on first render due to server-side rendering
+  const [size, setSize] = useState<Size>({ width: 100, height: 100, });
 
   const updateWidth = useCallback(() => {
     setSize(document.body.getBoundingClientRect());
