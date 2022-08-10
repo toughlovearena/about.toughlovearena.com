fda996212eb4520885e3dbc5557948b1d46400a1
mpaulweeks <mpaulweeks@gmail.com>
2022-08-10 15:48:24 -0400

    migrate to nps

---
+++ package-scripts.js
@@ -0,0 +1,9 @@
+module.exports = {
+  scripts: {
+    dev: 'next',
+    build: 'next build && next export',
+    default: 'next start',
+    typeCheck: 'tsc',
+    update: 'npx npm-check-updates -u',
+  },
+};
