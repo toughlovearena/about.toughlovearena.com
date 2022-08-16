e46fae02c0a60106bdaa2d9a46f1b3dde076bee7
mpaulweeks <mpaulweeks@gmail.com>
2022-08-15 21:34:34 -0400

    fix og:title to be based on page

--- components/layout/Layout.tsx
+++ components/layout/Layout.tsx
@@ -10,68 +10,67 @@ import styles from './Layout.module.css';
 
 type Props = {
   children: ReactNode;
-  title?: React.ReactNode;
   page?: InternalPageData;
   hideTitle?: boolean;
   pattern?: boolean;
 }
 
-const Meta = {
-  Favicon: '/favicon.png',
-  Description: "Tough Love Arena is a web-based, indie fighting game with rollback netcode that's 100% free to play.",
-} as const;
 
-const Layout = (props: Props) => (
-  <div>
-    <Head>
-      <title>
-        {(
-          props.title ||
-          (props.page?.label && `${props.page.label} | Tough Love Arena`) ||
-          `Tough Love Arena`
-        )}
-      </title>
-      <meta charSet="utf-8" />
-      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+const Layout = (props: Props) => {
+  const Meta = {
+    Title: (
+      (props.page?.label && `${props.page.label} | Tough Love Arena`) ||
+      `Tough Love Arena`
+    ),
+    Favicon: '/favicon.png',
+    Description: "Tough Love Arena is a web-based, indie fighting game with rollback netcode that's 100% free to play.",
+  } as const;
+  return (
+    <div>
+      <Head>
+        <title>{Meta.Title}</title>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
 
-      {/* meta */}
-      <meta name="description" content={Meta.Description} />
-      <link rel="shortcut icon" href={Meta.Favicon} />
-      <link rel="apple-touch-icon" href={Meta.Favicon} />
-      <meta name="theme-color" content="#000000" />
+        {/* meta */}
+        <meta name="description" content={Meta.Description} />
+        <link rel="shortcut icon" href={Meta.Favicon} />
+        <link rel="apple-touch-icon" href={Meta.Favicon} />
+        <meta name="theme-color" content="#000000" />
 
-      {/* Open Graph https://ogp.me/ */}
-      <meta property="og:title" content="Tough Love Arena" />
-      <meta property="og:description" content={Meta.Description} />
-      <meta property="og:type" content="website" />
-      <meta property="og:url" content="https://about.toughlovearena.com/" />
-      <meta property="og:image" content="https://about.toughlovearena.com/favicon.png" />
+        {/* Open Graph https://ogp.me/ */}
+        <meta property="og:title" content={Meta.Title} />
+        <meta property="og:description" content={Meta.Description} />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://about.toughlovearena.com/" />
+        <meta property="og:image" content="https://about.toughlovearena.com/favicon.png" />
 
-      {/* stylesheets */}
-      <link rel="stylesheet" href="/styles.css" />
-    </Head>
-    <div className={[styles.container, props.pattern ? styles.pattern : ''].join(' ')}>
-      <div className={styles.inner}>
-        <header className={styles.header}>
-          <NavDesktop currentHref={props.page?.href} links={InternalPages} />
-          <NavMobile currentHref={props.page?.href} links={InternalPages} />
-        </header>
-        <main className={styles.main}>
-          {props.page && !props.hideTitle && (
-            <SectionTitle>
-              {props.page.label}
-            </SectionTitle>
-          )}
-          {props.page?.subtitle && (
-            <Subtitle>
-              {props.page.subtitle}
-            </Subtitle>
-          )}
-          {props.children}
-        </main>
+        {/* stylesheets */}
+        <link rel="stylesheet" href="/styles.css" />
+      </Head>
+      <div className={[styles.container, props.pattern ? styles.pattern : ''].join(' ')}>
+        <div className={styles.inner}>
+          <header className={styles.header}>
+            <NavDesktop currentHref={props.page?.href} links={InternalPages} />
+            <NavMobile currentHref={props.page?.href} links={InternalPages} />
+          </header>
+          <main className={styles.main}>
+            {props.page && !props.hideTitle && (
+              <SectionTitle>
+                {props.page.label}
+              </SectionTitle>
+            )}
+            {props.page?.subtitle && (
+              <Subtitle>
+                {props.page.subtitle}
+              </Subtitle>
+            )}
+            {props.children}
+          </main>
+        </div>
       </div>
     </div>
-  </div>
-)
+  );
+}
 
 export default Layout
