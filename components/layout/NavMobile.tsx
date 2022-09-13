774a505548286249fad67b384326b1a468d21cee
mpaulweeks <mpaulweeks@gmail.com>
2022-09-13 18:27:06 -0400

    fix links on NavMobile

--- components/layout/NavMobile.tsx
+++ components/layout/NavMobile.tsx
@@ -1,6 +1,7 @@
 import Link from 'next/link'
 import { InternalPage } from '../../data/nav';
 import { NavProps } from '../../interfaces';
+import { ExternalLink } from '../ExternalLink';
 import styles from './NavMobile.module.css';
 
 export default ({ currentHref, links, }: NavProps) => (
@@ -8,21 +9,30 @@ export default ({ currentHref, links, }: NavProps) => (
     <nav className={styles.nav}>
       <div className={styles.navLogo}>
         <Link href={InternalPage.Home.href}>
-          <img src="/logo_v01.png" />
+          <a className={styles.navAnchor}>
+            <img src="/logo_v01.png" />
+          </a>
         </Link>
       </div>
       <div className={styles.navLinks}>
         {links.map((li, i) => (
           <Link key={i} href={li.href}>
-            <div className={styles.navLink}>
-              {currentHref === li.href ? (
-                <b>{li.label}</b>
-              ) : (
-                <span>{li.label}</span>
-              )}
-            </div>
+            <a className={styles.navAnchor}>
+              <div className={styles.navLink}>
+                {currentHref === li.href ? (
+                  <b>{li.label}</b>
+                ) : (
+                  <span>{li.label}</span>
+                )}
+              </div>
+            </a>
           </Link>
         ))}
+        <ExternalLink className={styles.navAnchor} href="https://toughlovearena.com">
+          <div className={styles.navLink}>
+            Play Now!
+          </div>
+        </ExternalLink>
       </div>
     </nav>
   </div>
